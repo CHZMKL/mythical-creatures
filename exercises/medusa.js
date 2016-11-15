300b0c25f40530bfe3d3108bf328ab7f0d617ea4
Chaz-Mykl <chaz.mykl@gmail.com>
2016-11-15 09:49:47 -0500

    Medusa in progress.

--- exercises/medusa.js
+++ exercises/medusa.js
@@ -1,9 +1,9 @@
 function Medusa(name) {
   this.name = name;
   this.statues = [];
-  this.victim = 0;
   this.stare = function() {
-
+    this.victim = victim;
   };
-  }
+}
+
 module.exports = Medusa;
